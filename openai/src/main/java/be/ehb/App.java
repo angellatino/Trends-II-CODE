74b0761b8d447018a0d34b0cf0ec7d7ad698605b
NdizeyeArmand <123093093+NdizeyeArmand@users.noreply.github.com>
2023-06-03 05:43:53 +0200

    Folderstructuur aangepast

---
+++ openai/src/main/java/be/ehb/App.java
@@ -0,0 +1,25 @@
+package be.ehb;
+
+import com.theokanning.openai.service.OpenAiService;
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.CompletionResult;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String apiKey = "YOUR_API_KEY_HERE";
+        OpenAiService service = new OpenAiService(apiKey);
+        
+        CompletionRequest request = CompletionRequest.builder()
+            .prompt("Wat is Erasmushogeschool Brussel? Wees bondig!")
+            .model("text-davinci-003")
+            .maxTokens(300)
+            .build();
+        CompletionResult response = service.createCompletion(request); String generatedText = response.getChoices().get(0).getText(); System.out.println(generatedText);
+    }
+}
