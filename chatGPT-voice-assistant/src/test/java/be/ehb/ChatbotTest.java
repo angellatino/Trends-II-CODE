e69fd5637e4245eb99e10e7902314f9cff1d21dc
NdizeyeArmand <123093093+NdizeyeArmand@users.noreply.github.com>
2023-06-22 23:03:22 +0200

    Code in één klasse gestoken

---
+++ chatGPT-voice-assistant/src/test/java/be/ehb/ChatbotTest.java
@@ -0,0 +1,20 @@
+package be.ehb;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ChatbotTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
